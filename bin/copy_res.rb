a5b0e7b36c3fa436a7b81ae8574edacdc516faf2
Davide Ficano <davide.ficano@gmail.com>
2015-01-05 15:36:37 +0100

    fixed regression about copying files

--- bin/copy_res.rb
+++ bin/copy_res.rb
@@ -25,7 +25,7 @@ def copy_res(res_from_path, drawable_root_dest_dir, basename = nil)
             src_path = res_from_path.gsub('$path$', v);
             full_path = File.join(full_path, filename)
             # puts "cp #{src_path} #{full_path}"
-            # FileUtils.cp(src_path, full_path)
+            FileUtils.cp(src_path, full_path)
         end
     end
 end
@@ -59,17 +59,17 @@ end
 
 def parse_command_line()
     cmd_opts = OpenStruct.new
-    cmd_opts.base_dest_dir = '../../photoshelf/app/src/main/res'
-    cmd_opts.src_res_dir = '../material-design-icons'
+    cmd_opts.dest_dir = '../../photoshelf/app/src/main/res'
+    cmd_opts.src_dir = '../material-design-icons'
 
     optparser = OptionParser.new do |opts|
         opts.banner = "Usage: #{File.basename($0)} [options] config-file"
         opts.separator ''
-        opts.on('-b', '--base base dir', 'The drawable-XXXX base destination directory') do |value|
-            cmd_opts.src_res_dir = value
+        opts.on('-d', '--dest root_dir', 'The root drawable-XXXX destination directory') do |value|
+            cmd_opts.dest_dir = value
         end
-        opts.on('-s', '--src dir', 'The source directory containing the resources to copy') do |value|
-            cmd_opts.base_dest_dir = value
+        opts.on('-s', '--src root_dir', 'The source directory containing the resources to copy') do |value|
+            cmd_opts.src_dir = value
         end
         opts.on_tail('-h', '--help', 'This help text') do
             puts opts
@@ -86,8 +86,8 @@ def parse_command_line()
     end
     json_map = JSON.parse(open(ARGV[0]).read)
     options = OpenStruct.new(json_map)
-    options.base_dest_dir = cmd_opts.base_dest_dir
-    options.src_res_dir = cmd_opts.src_res_dir
+    options.dest_dir = cmd_opts.dest_dir
+    options.src_dir = cmd_opts.src_dir
 
     return options
 end
@@ -96,11 +96,11 @@ options = parse_command_line()
 
 options.resources.each do |k, v|
     next if k.start_with?('==>DISABLE_THIS')
-    res_pattern = find_first_path(options.src_res_dir, k)
+    res_pattern = find_first_path(options.src_dir, k)
     if res_pattern.nil?
         puts "Unable to find #{k}"
         next
     end
     res_pattern.gsub!(/drawable-.*[\/\\]/, '$path$/')
-    copy_res(res_pattern, options.base_dest_dir, v)
+    copy_res(res_pattern, options.dest_dir, v)
 end
