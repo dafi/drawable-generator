f463f17c4d51105dfcd63902739361d8752ff103
Davide Ficano <davide.ficano@gmail.com>
2015-02-14 10:57:12 +0100

    No longer used but it's useful to understard how to use unix imagemagik

--- bin/svg2png.sh
+++ bin/svg2png.sh
@@ -20,11 +20,15 @@ svg_to_png() {
 base_dir=../../photoshelf/app/src/main/res
 base_dir=../tmp
 svg_file=../svg/draft_48px.svg
+# svg_file=../tmp/converted.svg
 
-svg_to_png 24 300 $base_dir/drawable-mdpi $svg_file
-svg_to_png 36 300 $base_dir/drawable-hdpi $svg_file
-svg_to_png 48 300 $base_dir/drawable-xhdpi $svg_file
-svg_to_png 72 300 $base_dir/drawable-xxhdpi $svg_file
+# svg_to_png 24 300 $base_dir/drawable-mdpi $svg_file
+# svg_to_png 36 300 $base_dir/drawable-hdpi $svg_file
+# svg_to_png 48 300 $base_dir/drawable-xhdpi $svg_file
+# svg_to_png 72 300 $base_dir/drawable-xxhdpi $svg_file
+
+convert -background transparent ../tmp/converted.svg ../tmp/vediamo.png
+    
 
 # convert output.png -color-matrix \
 #      " 1.5 0.0 0.0 0.0, 0.0, -0.157 \
