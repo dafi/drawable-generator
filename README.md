d7921ac3b153105cf104f9bb486366ab36e5bae5
Davide Ficano <davide.ficano@gmail.com>
2017-02-26 10:25:50 +0100

    generate all drawables

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 Used to simplify resource copy and generation (svg to png) 
 run setup.sh to pull used repositories
+
+Example
+
+Create all drawables `40x40, 60x60, 80x80, 120x120, 160x160`
+
+	baseOutputDir/drawable-mdpi/my.png
+	baseOutputDir/drawable-hdpi/my.png
+	baseOutputDir/drawable-xhdpi/my.png
+	baseOutputDir/drawable-xxhdpi/my.png
+	baseOutputDir/drawable-xxxhdpi/my.png
+
+starting from the `my.svg` file
+
+	run.sh 40 300 baseOutputDir my.svg
+	
--- bin/svg2png.sh
+++ bin/svg2png.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
 
+density_array=(1 1.5 2 3 4)
+drawable_dir=(drawable-mdpi drawable-hdpi drawable-xhdpi drawable-xxhdpi drawable-xxxhdpi)
+size=${1:-24}
+density=${2:-300}
+
 svg_to_png() {
-    size=$1x$1
-    density=$2
-    dest_path=$3
-    filename=$4
+    local size=$1x$1
+    local density=$2
+    local dest_path=$3
+    local filename=$4
 
     # remove the dimension from destination filename
-    out_filename=${filename//_[0-9]*px/}
-    out_filename=ic_action_${out_filename//svg/png}
+    local out_filename=${filename//_[0-9]*px/}
+    local out_filename=ic_action_${out_filename//svg/png}
     mkdir -p $dest_path
 
     convert -density $density -background none \
     -fill white -opaque black \
     $filename \
     -resize $size -gravity center -extent $size $dest_path/`basename $out_filename`
+
+    echo created $dest_path/`basename $out_filename`
 }
 
+
+
 base_dir=../../photoshelf/app/src/main/res
-base_dir=../tmp
 svg_file=../svg/draft_48px.svg
 # svg_file=../tmp/converted.svg
+
+# base_dir=../tmp
+svg_file=../svg/bookmark_border.svg
+
+for i in "${!density_array[@]}"; do
+    size_density=`bc <<< "scale=0;($size*${density_array[$i]})/1"`
+    svg_to_png $size_density $density $base_dir/${drawable_dir[$i]} $svg_file
+done
+
+# svg_to_png 40 300 ../tmp/drawable-mdpi ../svg/bookmark_green.svg
+
+# svg_to_png 40 300 $base_dir/drawable-mdpi $svg_file
+# svg_to_png 80 300 $base_dir/drawable-hdpi $svg_file
+# svg_to_png 160 300 $base_dir/drawable-xhdpi $svg_file
+# svg_to_png 320 300 $base_dir/drawable-xxhdpi $svg_file
 
 # svg_to_png 24 300 $base_dir/drawable-mdpi $svg_file
 # svg_to_png 36 300 $base_dir/drawable-hdpi $svg_file
 # svg_to_png 48 300 $base_dir/drawable-xhdpi $svg_file
 # svg_to_png 72 300 $base_dir/drawable-xxhdpi $svg_file
 
-convert -background transparent ../tmp/converted.svg ../tmp/vediamo.png
+# convert -background transparent ../tmp/converted.svg ../tmp/vediamo.png
     
 
 # convert output.png -color-matrix \
